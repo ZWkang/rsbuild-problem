4a2a842a5596e8dec0a17c3a13790b4c5593493e
zwkang <kang95630@gmail.com>
2024-05-10 22:01:52 +0800

    feat: init

--- rsbuild.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from '@rsbuild/core';
-import { pluginBabel } from '@rsbuild/plugin-babel';
-import { pluginVue } from '@rsbuild/plugin-vue';
-
-export default defineConfig({
-  plugins: [pluginVue()]
-});
